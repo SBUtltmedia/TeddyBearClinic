e5b9b287252a15a1c8c8baec4dc9f25d801e10b3
Paul St. Denis <pstdenis@gmail.com>
2021-05-22 21:41:11 -0400

    fixing bubble

--- js/ModalDialog.js
+++ js/ModalDialog.js
@@ -59,7 +59,7 @@ class ModalDialog {
     this.popup = $("<div/>", {
       id: "thoughtBubble",
       class: "thoughtPopAnimation"
-    }).load("newPopup.svg",()=>this.populateBubble())
+    }).load("popup.html",()=>this.populateBubble())
     
 
 
