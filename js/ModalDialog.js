a093dfc6be0271599f426d9f726d3a2a7e674fd1
TLT Media Lab <tltmedialab@connect.stonybrook.edu>
2021-05-25 14:37:09 -0400

    May twenty fifth, made is good and is good bubble

--- js/ModalDialog.js
+++ js/ModalDialog.js
@@ -8,7 +8,7 @@ class ModalDialog {
     this.textType = textType;
     this.audioPlay = this.room.house.autoplay ? 'audioOn' : 'audioOff';
     this.displayTargetInfo();
-
+    console.log(target)
 
   }
 
@@ -59,47 +59,67 @@ class ModalDialog {
     this.popup = $("<div/>", {
       id: "thoughtBubble",
       class: "thoughtPopAnimation"
-    }).load("popup.html",()=>this.populateBubble())
-    
+    }).load("popup.html", () => this.populateBubble())
+
 
 
   }
-  populateBubble(){
+  closeBubble() {
+    
+    $("#thoughtBubble").remove()
+    this.room.removeTarget()
+    if (!this.room.triggersLeft) {
+      this.room.house.currentRoom = this.room.roomInfo.nextRoom;
+      this.room.house.isTutorial = false;
+      setTimeout(() => this.room.house.loadRoom(), 1000)
+
+
+    }
+  }
+  changeBubble() {
+    $("#thoughtBubble").hide()
+    this.callback.animate().then(() => {
+      $("#Heading").html(this.heading)
+
+
+      $("#bubbleContent").html(this.target[this.textType][1])
+
+      $(".background1Border").removeClass("background1Border").addClass(this.border)
+      $(".background1").removeClass("background1").addClass(this.background)
+      $("#thoughtBubble").show()
+    })
+  }
+  populateBubble() {
     $("#roomSVG").append(this.popup)
-    $("#bubbleHeaders").html("This is not safe!")
+    console.log(this.target.isGood)
+    if(this.target.isGood) {
+      console.log("Bananas")
+      this.background="backgroundGood"
+      this.border="backgroundGoodBorder"
+      this.heading="Good job!"
+      this.changeBubble()
+    }
+    else{
+      this.background="background2"
+      this.border="background2Border"
+      this.heading="This is not safe!"
+    }
+    $("#Heading").html(this.heading) 
     console.log(this)
-   
+
     $("#bubbleContent").html(this.target[this.textType][0])
 
 
-    $("#thoughtBubble").on("click", ()=> {
-      if ( $(".background2").length) {
-        $("#thoughtBubble").remove()
-        this.room.removeTarget()
-        if (!this.room.triggersLeft) {
-          this.room.house.currentRoom = this.room.roomInfo.nextRoom;
-          this.room.house.isTutorial = false;
-          setTimeout(() => this.room.house.loadRoom(), 1000)
-  
-  
-        }
+    $("#thoughtBubble").on("click", () => {
+      if ($(".background1").length) {
+        this.heading="What do we do?"
+    this.changeBubble() 
       }
       else {
-        $("#thoughtBubble").hide()
-        this.callback.animate().then(()=>
-       {
-        $("#Title_1_").html("What do we do?")
-    
-     
-      $("#bubbleContent").html(this.target[this.textType][1])
-      
-        $(".background1Border").removeClass("background1Border").addClass("background2Border")
-        $(".background1").removeClass("background1").addClass("background2")
-        $("#thoughtBubble").show()
-      })
-    }
+        this.closeBubble()
+      }
 
-   
+
     })
   }
 }
